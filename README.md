cbcf0a9e62d7271b01787b8322824f650075539d
meddlin <drushing.dev@gmail.com>
2023-07-09 23:48:35 -0500

    Using maps now

--- README.md
+++ README.md
@@ -51,6 +51,26 @@ use (
 
 - Go slices: [https://go.dev/blog/slices-intro](https://go.dev/blog/slices-intro)
 
+### Maps
+
+Maps are Go's associative data type. They are commonly used to store key-value pairs.
+
+Setting them up, they follow this pattern:
+
+```go
+make(map[key-type]val-type)
+
+/*
+ * A map where the key is a string, and the value is a string
+ * -- similar to JSON -- would look like this.
+*/
+make(map[string]string)
+```
+
+- Resources
+  - "Go by example": [https://gobyexample.com/maps](https://gobyexample.com/maps)
+  - go.dev: [https://go.dev/blog/maps](https://go.dev/blog/maps)
+
 
 ### Seeding the random package
 
--- ex2/greetings/greetings.go
+++ ex2/greetings/greetings.go
@@ -27,15 +27,22 @@ func Hello(name string) (string, error) {
 	return message, nil
 }
 
+// Hellos returns a map that associates each of the named people
+// with a greeting message.
 func Hellos(names []string) (map[string]string, error) {
+	// A map to associate names with messages.
 	messages := make(map[string]string)
 
-	for _, name := range names {
+	// Loop through the received slice of names, calling
+	// the Hello function to get a message for each name.
+	for _, name := range names { // We didn't need the index, so we used the blank identifier.
 		message, err := Hello(name)
 		if err != nil {
 			return nil, err
 		}
 
+		// In the map, associate the retrieved message with
+		// the name.
 		messages[name] = message
 	}
 
