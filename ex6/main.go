8a45aa305429910d0c58fb40e70d56b7649d4a81
meddlin <drushing.dev@gmail.com>
2023-07-11 13:29:25 -0500

    ex6 - Setup exercise for io package, using defer

--- ex6/main.go
+++ ex6/main.go
@@ -2,8 +2,39 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
 	fmt.Println("EX6 - Making use of io package")
+
+	f2, err := os.Open(".\\data.csv")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f2.Close()
+	// DoSomething(f, f2)
+	DoSomething(f2)
+}
+
+// func DoSomething(source io.ReadSeeker, destination io.WriteSeeker) {
+// 	io.Copy(destination, source)
+// }
+
+func DoSomething(source io.Reader) {
+	fmt.Print("Copying from source to destination...")
+	io.Copy(os.Stdout, source)
+}
+
+func closeFile(f *os.File) {
+	fmt.Println("closing")
+	err := f.Close()
+
+	// it's good to check for errors when closing a file,
+	// even though we deferred it earlier.
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
 }
---
+++ ex6/notes.md
@@ -0,0 +1,8 @@
+# Using the io package
+
+
+Defer
+
+"Defer is used to ensure that a function call is performed later in a program's execution..."
+
+- Ref: [https://gobyexample.com/defer](https://gobyexample.com/defer)
