a0c192e0db10d26da6e4bc5f942e942e42bf42a5
meddlin <drushing.dev@gmail.com>
2023-07-11 16:12:17 -0500

    An example using channels

--- ex5/main.go
+++ ex5/main.go
@@ -4,6 +4,36 @@ import (
 	"fmt"
 )
 
+/*
+ * A function, that accepts a channel as a parameter
+ * - Prints to stdout
+ * - Sends a message to the channel
+ */
+func printMessage(ch chan string) {
+	fmt.Println("Hello, from our printMessage")
+	ch <- "Sending to channel...from printMessage"
+}
+
 func main() {
 	fmt.Println("EX5 - Context")
+
+	// Let's make a channel
+	ch := make(chan string)
+
+	// And we're making an inline goroutine
+	go func() {
+		fmt.Println("Hello, from our inline goroutine")
+		// ...let's send a message to our channel
+		ch <- "Sending to channel...from inline goroutine"
+	}()
+
+	go printMessage(ch)
+	fmt.Println("Hello, from our main()")
+
+	// Let's read from our channel
+	message := <-ch
+	fmt.Println("from the channel: ", message)
+
+	// Throwaway the second value because...happy birthday to the ground
+	<-ch
 }
