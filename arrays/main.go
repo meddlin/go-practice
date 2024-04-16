f74c7ac520478af8ea60c03511efdb142e14ec57
meddlin <drushing.dev@gmail.com>
2024-04-15 22:56:07 -0500

    loop and messages

--- arrays/main.go
+++ arrays/main.go
@@ -5,12 +5,24 @@ import "fmt"
 func main() {
 	/* Arrays */
 	// First we create two arrays and print them
+	fmt.Println()
+	fmt.Println("** printing an array of strings... **")
 	var a [2]string
 	a[0] = "hello"
 	a[1] = "world"
 	fmt.Println(a[0], a[1])
 	fmt.Println(a)
 
+	fmt.Println()
+	fmt.Println("** printing an array of ints... **")
 	primes := [6]int{2, 3, 5, 7, 11, 13}
 	fmt.Println(primes)
+
+	/* Loop over an array */
+	fmt.Println()
+	fmt.Println("** looping over an array... **")
+
+	for i := 0; i < len(primes); i++ {
+		fmt.Println(primes[i])
+	}
 }
