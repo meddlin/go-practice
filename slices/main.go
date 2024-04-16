6a32fe8b1114486182d25b0ae24b21aeaab15137
meddlin <drushing.dev@gmail.com>
2024-04-15 22:50:27 -0500

    slices area

---
+++ slices/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+
+	var s []int = primes[1:4]
+
+	fmt.Println("The slice is...")
+	fmt.Println(s)
+
+	fmt.Println()
+	fmt.Println("The full array is...")
+	fmt.Println(primes)
+}
