813374feffb6f790cbb5a8f5ee49e8c2031881b4
meddlin <drushing.dev@gmail.com>
2024-04-16 00:35:01 -0500

    loop over even, odd

--- arrays/main.go
+++ arrays/main.go
@@ -23,7 +23,7 @@ func main() {
 	fmt.Println("** looping over an array... **")
 
 	for i := 0; i < len(primes); i++ {
-		fmt.Printf("%d", primes[i])
+		fmt.Printf("%d\n", primes[i])
 	}
 
 	/* Loop with 'range' */
@@ -31,5 +31,23 @@ func main() {
 	fmt.Println("** loop over array using 'range'... **")
 	for _, num := range primes {
 		fmt.Println(num)
+	}
+
+	/* Print every even number */
+	fmt.Println()
+	fmt.Println("** printing every *even* number in an array... **")
+	smallArr := [10]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	for i := 0; i < len(smallArr); i++ {
+		if smallArr[i]%2 == 0 {
+			fmt.Printf("[%d]: %d\n", i, smallArr[i])
+		}
+	}
+
+	/* Calculate int's in an array, and print the odd values */
+	funArr := [5]int{0, 4, 5 * 3, 12, 3 / 2}
+	for _, p := range funArr {
+		if p%2 != 0 {
+			fmt.Printf("odd -> %d\n", p)
+		}
 	}
 }
